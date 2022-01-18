8a58781834cd79994b7454077cc38bed623b12e0
Samuel Silva <samuelformal@gmail.com>
2022-01-18 13:05:26 -0300

    Add new page to verify how next works with SSR

---
+++ src/pages/blog.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Link from "next/link"
+
+export default () => (
+  <div>
+    <Link href={'/'}>
+      <a>Homepage</a>
+    </Link>
+    <h1>Blog do Samuel</h1>
+  </div>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 import 'isomorphic-fetch'
 
 const Home = ({ repositories }) => (
   <div>
+    <Link href={'/blog'}>
+      <a>Blog</a>
+    </Link>
     {repositories?.map(repo => (
       <h3 key={repo.id}>{`Repository ${repo.id}: ${repo.name}`}</h3>
     ))}
@@ -12,6 +16,9 @@ const Home = ({ repositories }) => (
 Home.getInitialProps = async () => {
   const response = await fetch('https://api.github.com/users/samuelcrx/repos')
   const repositories = await response.json()
+  
+  console.log('Teste de renderização')
+
   return { repositories }
 }
 
